ac4d830554d5c49c2ec342314444d030b5beacf4
James Garlit <garlitjamessevilla@gmail.com>
2023-05-02 02:08:10 +0800

    update

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -5,12 +5,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>ChartJS</h1>
+            <h1>Analytics</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">ChartJS</li>
+              <li class="breadcrumb-item active">Analytics</li>
             </ol>
           </div>
         </div>
--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -5,12 +5,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>FAQ</h1>
+            <h1>Help</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">FAQ</li>
+              <li class="breadcrumb-item active">Help</li>
             </ol>
           </div>
         </div>
